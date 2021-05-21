c12bba59b2a25cde1c7b07d2290da0a95210ee7f
10006720 <zhang371312@126.com>
2021-05-21 14:02:02 +0800

    [feat]: 1672. 最富有客户的资产总量
    44 ms, 40.38%
    14.8 MB, 76.91%

---
+++ src/LeetCode/maximumWealth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File           : maximumWealth
+@Software       : PyCharm
+@Modify Time    : 2021/5/21 13:57
+@Author         : zermzhang
+@version        : 1.0
+@Desciption     :
+"""
+from typing import List
+
+
+def maximumWealth(accounts: List[List[int]]) -> int:
+    user_wealth = []
+    for i in range(len(accounts)):
+        user_wealth.append(sum(accounts[i]))
+    return max(user_wealth)
+
+
+if __name__ == '__main__':
+    accounts_ = [[1,5],[7,3],[3,5]]
+    print(maximumWealth(accounts_))
