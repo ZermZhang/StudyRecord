b4e75f5ad5c4a1880e976f0beedc68e3cd64a14c
zermzhang <zhang371312@126.com>
2021-08-05 17:31:33 +0800

    [feat]: 802. 找到最终的安全状态

--- src/LeetCode/eventualSafeNodes.py
+++ src/LeetCode/eventualSafeNodes.py
@@ -27,10 +27,31 @@ class Solution:
 
         return ends
 
+    def eventualSafeNodes_v2(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        visited = set()
+        ans = []
+
+        def dfs(node: int) -> bool:
+            if node in visited:
+                return False
+            visited.add(node)
+            for neighbor in graph[node]:
+                if not dfs(neighbor):
+                    return False
+            graph[node] = []
+            visited.remove(node)
+            return True
+
+        for i in range(n):
+            if dfs(i):
+                ans.append(i)
+        return ans
+
 
 if __name__ == "__main__":
     solution = Solution()
     graph = [[1, 2], [2, 3], [5], [0], [5], [], []]
 
-    safe_nodes = solution.eventualSafeNodes(graph)
+    safe_nodes = solution.eventualSafeNodes_v2(graph)
     print(f"the all safe nodes: {safe_nodes}")
