970616cb3ddbe9359cb8ae0ef4f9200fc68ecab0
ZermZhang <zhang371312@126.com>
2023-01-31 09:37:46 +0800

    [docs]: 增加注释

--- src/LeetCode/bestTeamScore.py
+++ src/LeetCode/bestTeamScore.py
@@ -3,11 +3,14 @@ from typing import List
 
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        # 将年龄和得分组合后按照年龄升序进行排序
         ans = sorted(zip(ages, scores))
+        # 保留按照年龄升序的得分序列
         dp = [i[1] for i in ans]
 
         for i in range(1, len(ans)):
             for j in range(i):
+                # 如果当前得分大于前面的得分，则进行累加
                 if ans[i][1] >= ans[j][1]:
                     dp[i] = max(dp[i], dp[j]+ans[i][1])
 
