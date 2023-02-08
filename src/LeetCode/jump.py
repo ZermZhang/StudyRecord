71894643f9cafea2fc8b60d662f6bcf31e8ad9bc
ZermZhang <zhang371312@126.com>
2023-02-08 09:07:08 +0800

    45. Jump Game II

---
+++ src/LeetCode/jump.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # 从0开始，每一步都要跳的做够多
+        # 如果这一步跳过的所有ele中，包含能够直接跳到尾部的长度即可
+        idx, new_idx, step = 0, 0, 0
+
+        for i in range(len(nums) - 1):
+            new_idx = max(new_idx, i + nums[i])
+            if idx == i:
+                idx = new_idx
+                step += 1
+        return step
+
+        # if len(nums) == 1:
+        #     return 1
+        # if nums[idx] >= len(nums) - 1:
+        #     return 1
+
+        # while idx < len(nums):
+        #     if idx + nums[idx] >= len(nums) - 1:
+        #         return step
+        #     new_idx = idx + nums[idx]
+        #     print(idx, new_idx)
+        #     for j in range(idx + 1, idx + nums[idx]):
+        #         print('\t\t', j, idx + j + nums[j])
+        #         if new_idx > idx + j + nums[j]:
+        #             continue
+        #         else:
+        #             new_idx = idx + j + nums[j]
+        #     idx = new_idx
+        #     step = step + 1
+        #     print('\t', idx, step)
+
+        return step
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    # nums = [1, 1, 1, 1]
+    # nums = [2, 3, 1, 1, 4]
+    # nums = [1, 1, 1, 1, 1]  # 4
+    nums = [3, 4, 3, 2, 5, 4, 3]  # 3
+
+    print(sol.jump(nums=nums))
