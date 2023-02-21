007ac1d31b176e9b47974b64b1e52d7bb5a5a5ee
ZermZhang <zhang371312@126.com>
2023-02-21 08:14:29 +0800

    35. Search Insert Position

---
+++ src/LeetCode/Sorted/searchInsert.py
@@ -0,0 +1,23 @@
+from typing import *
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums)-1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return end+1
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = [1, 4]
+    target = 1
+    print(sol.searchInsert(nums, target))
