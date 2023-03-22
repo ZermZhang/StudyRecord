1dfd7137dc308d291e3c1d7b3ebd01952da31da0
ZermZhang <zhang371312@126.com>
2023-03-22 09:11:10 +0800

    增加文档说明

--- src/LeetCode/DP/maxProfit.py
+++ src/LeetCode/DP/maxProfit.py
@@ -16,11 +16,15 @@ class Solution:
         max_profit = 0
         left = 0
 
+        # 声明双指针，左侧标明最低价、右侧遍历所有的价格
+        # 右侧的指针逐渐走动，左侧的指针根据条件进行变动
+        # 如果出现了一个比left价格更低的情况，进行调整
         for right in range(1, len(prices)):
             if prices[right] < prices[left]:
                 left = right
                 continue
             else:
+                # 计算最大利润并记录
                 max_profit = max(max_profit, prices[right] - prices[left])
         return max_profit
 
