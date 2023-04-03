e680bfdc40e24fa396390c331c17bfabe53d130a
ZermZhang <zhang371312@126.com>
2023-04-03 08:46:47 +0800

    [docs]

--- src/LeetCode/SUM/numRescueBoats.py
+++ src/LeetCode/SUM/numRescueBoats.py
@@ -13,16 +13,28 @@ from typing import List
 
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
+        """
+        :param people:
+        :param limit:
+        :return:
+        """
+        # 先对人员的信息进行排序处理，从大到小或者从小到大
         sorted_poeple = sorted(people)
         nums = 0
+        # 使用双指针对数组进行处理
         left, right = 0, len(sorted_poeple) - 1
 
         while left < right:
+            # 计算当前两个指针分别指向的结果是否能够满足limit
             remain = limit - sorted_poeple[right]
+            # 如果不能满足的话，说明右侧指针的结果已经是偏大了，无法和最小的值匹配上
+            # 所以right对应的结果自己就要占据一个位置
+            # right - 1，left不动
             if remain < sorted_poeple[left]:
                 nums += 1
                 right -= 1
             else:
+                # 满足要求的话，可以让left和right都走一步
                 nums += 1
                 right -= 1
                 left += 1
