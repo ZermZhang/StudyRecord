9f51b778b07814bbca04dc05c964bd2935ff4957
ZermZhang <zhang371312@126.com>
2023-04-10 19:06:49 +0800

    [add]: 20230410

---
+++ src/LeetCode/Sorted/prevPermOpt1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File           : prevPermOpt1
+@Software       : PyCharm
+@Modify Time    : 2023/4/3 08:48
+@Author         : zermzhang
+@version        : 1.0
+@Desciption     :
+"""
+from typing import List
+
+
+class Solution:
+    def prevPermOpt1(self, arr: List[int]) -> List[int]:
+
+
+
+        return arr
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    arr = [3, 2, 1]
+    print(sol.prevPermOpt1(arr))
